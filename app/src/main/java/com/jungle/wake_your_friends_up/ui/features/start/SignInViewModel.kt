50fabde50833059422743dcc161b7d6ad4bb9923
oguzevirgen <evirgenoguz@gmail.com>
2023-03-03 15:16:48 +0300

    add | start screens design

---
+++ app/src/main/java/com/jungle/wake_your_friends_up/ui/features/start/SignInViewModel.kt
@@ -0,0 +1,7 @@
+package com.jungle.wake_your_friends_up.ui.features.start
+
+import androidx.lifecycle.ViewModel
+
+class SignInViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
---
+++ app/src/main/java/com/jungle/wake_your_friends_up/ui/features/start/StartViewModel.kt
@@ -0,0 +1,7 @@
+package com.jungle.wake_your_friends_up.ui.features.start
+
+import androidx.lifecycle.ViewModel
+
+class StartViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}

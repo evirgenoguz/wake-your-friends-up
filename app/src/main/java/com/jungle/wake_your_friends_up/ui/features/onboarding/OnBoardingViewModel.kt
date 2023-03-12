dd42b8799f5658c877464a511ce074b06c6a4dc5
tasci <burak.tasci@appcent.mobi>
2023-03-12 17:48:16 +0300

    add | OnBoardingViewModel.kt

---
+++ app/src/main/java/com/jungle/wake_your_friends_up/ui/features/onboarding/OnBoardingViewModel.kt
@@ -0,0 +1,22 @@
+package com.jungle.wake_your_friends_up.ui.features.onboarding
+
+import android.graphics.Bitmap
+import android.os.Parcelable
+import com.jungle.wake_your_friends_up.core.BaseViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.parcelize.Parcelize
+import javax.inject.Inject
+
+@HiltViewModel
+class OnBoardingViewModel @Inject constructor() : BaseViewModel() {
+
+
+
+
+
+    @Parcelize
+    data class OnBoardingModel(
+        val title: String,
+        val contentImage: Bitmap,
+    ): Parcelable
+}

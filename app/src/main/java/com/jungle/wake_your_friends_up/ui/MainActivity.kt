22a1c49004a5af9666562e2f1103d39490652a46
tasci <burak.tasci@appcent.mobi>
2023-03-12 17:41:58 +0300

    add | navigation component

--- app/src/main/java/com/jungle/wake_your_friends_up/ui/MainActivity.kt
+++ app/src/main/java/com/jungle/wake_your_friends_up/ui/MainActivity.kt
@@ -4,6 +4,8 @@ import android.view.LayoutInflater
 import androidx.activity.viewModels
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import androidx.navigation.NavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.navigateUp
 import com.jungle.wake_your_friends_up.core.BaseActivity
 import com.jungle.wake_your_friends_up.databinding.ActivityMainBinding
 import com.jungle.wake_your_friends_up.ext.findNavController
@@ -13,22 +15,35 @@ import dagger.hilt.android.AndroidEntryPoint
 @AndroidEntryPoint
 class MainActivity : BaseActivity<ActivityMainBinding>() {
 
-
     private val viewModel by viewModels<MainViewModel>()
 
     private lateinit var navController: NavController
 
+    private lateinit var appBarConfiguration: AppBarConfiguration
+
     override val bindingInflater: (LayoutInflater) -> ActivityMainBinding
         get() = ActivityMainBinding::inflate
 
-    override fun setupUi() {
+    override fun preOnCreate() {
+        super.preOnCreate()
         // splash screen setup, it appears according
         // to MainViewModel's isLoading variable.
         setupSplashScreen()
+    }
 
+    override fun setupUi() {
         // getting NavController with findNavController
         // extension.
         navController = findNavController()
+
+        // Create the app bar configuration to support
+        // unhandled navigate up calls.
+        appBarConfiguration = AppBarConfiguration(navController.graph)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return navController.navigateUp(appBarConfiguration)
+                || super.onSupportNavigateUp()
     }
 
     private fun setupSplashScreen() {
